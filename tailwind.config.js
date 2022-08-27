735a575f10e067d894d62d25366deba18837fc43
cbHasib <hasibul.pbn@gmail.com>
2022-08-27 14:06:17 +0600

    Dark Mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
+
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+};
